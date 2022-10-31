6f42b09ad8e90fbb82f3df81d39cee7412d6a513
Marius <marius@example.com>
2022-10-31 10:26:27 +0200

    uzd 1 2

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 'use strict';
 console.log('main.js');
+
+// taikomes
+const pNumsEl = document.getElementById('nums');
+const nums = [5, 10, 1, 50, 12];
+// 1. i ` <p id="nums"></p>` surasyti skaicius is `const nums = [5, 10, 1, 50, 12];`
+const stringFromArray = nums.join(', ');
+pNumsEl.textContent = stringFromArray;
+
+// 2. paspaudus pelyte ant to paragrafo isrikiuoti skaicius didejancia tvarka
+pNumsEl.addEventListener('click', () => {
+  console.log('click');
+  nums.sort((x, y) => x - y);
+  pNumsEl.textContent = nums.join(', ');
+});
