6af4355322788d15e43cead11e0f7456b21af213
Marius <marius@example.com>
2022-10-31 10:53:13 +0200

    reduce total

--- js/reduce.js
+++ js/reduce.js
@@ -3,7 +3,7 @@ console.log('reduce.js');
 
 // array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
 
-const nums = [5, 10, 1, 50, 12];
+const nums = [5, 7, 1, 50, 12];
 
 // surasti bendra masyvo reiksmiu suma su forEach
 
@@ -13,11 +13,14 @@ nums.forEach((sk, i, arr) => {
   total = total + sk;
 });
 console.log('total ===', total);
-
+console.log('nums ===', nums);
 // surasti bendra masyvo reiksmiu suma su reduce
+// debugger;
 const reduceRezult = nums.reduce((total, sk, i, arr) => {
   // ispausdinti su konsole kiekvienos iteracijos:
   // indexa, total, sk
-  return total + sk; // 1
+  const sum = total + sk;
+  // console.log(`iteracija: ${i}, sk: ${sk}, total: ${sum}`);
+  return sum; // 1
 }, 0);
 console.log('reduceRezult ===', reduceRezult);
