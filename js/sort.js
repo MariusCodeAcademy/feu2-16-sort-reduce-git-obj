e8c67deb26e4e0fb506f2d08291b8f98bbe7e003
Marius <marius@example.com>
2022-10-31 09:53:11 +0200

    sort localCompare

--- js/sort.js
+++ js/sort.js
@@ -4,20 +4,20 @@ console.log('sort.js');
 const users = [
   {
     name: 'James',
-    age: 25,
+    age: 65,
     gender: 'male',
     town: 'London',
-  },
-  {
-    name: 'Mike',
-    age: 35,
-    gender: 'male',
-    town: 'Kaunas',
   },
   {
     name: 'Jane',
     age: 27,
     gender: 'female',
+    town: 'Kaunas',
+  },
+  {
+    name: 'Mike',
+    age: 35,
+    gender: 'male',
     town: 'Kaunas',
   },
   {
@@ -27,3 +27,28 @@ const users = [
     town: 'London',
   },
 ];
+
+console.table(users);
+
+// isrikiuoti pagal amziu didejanciai
+// users.sort((a, b) => a.age - b.age);
+// console.table(users);
+
+// isrikiuoti pagal amziu mazejanciai
+// users.sort((a, b) => b.age - a.age);
+// console.table(users);
+
+console.log(users[0].town > users[1].town);
+
+// rikiuojam pagal miesta
+users.sort((a, b) => (a.town > b.town ? 1 : -1));
+console.table(users);
+
+// rikiuojam pagal gender
+users.sort((a, b) => a.gender.localeCompare(b.gender));
+console.log('a.gender.localeCompare(b.gender)');
+console.table(users);
+
+// isrikiuoti musu masyva pagal varda
+// az
+// za
